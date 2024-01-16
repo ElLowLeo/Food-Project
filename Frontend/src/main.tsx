4855bc8a1d3d5fdc32a9d1a587c44ce31992fad9
Davi-html <davifernandes3674@gmail.com>
2024-01-16 18:04:54 -0300

    criando a page de signin

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Signup } from './pages/signup/index'
 import GlobalStyle from './styles/global.ts'
 import {ThemeProvider} from 'styled-components'
 
+import { Signin } from './pages/signin/index'
 import theme from './styles/theme.ts'
 
 
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
 <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Signup />
+      <Signin />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ Frontend/src/pages/signin/index.tsx
@@ -0,0 +1,31 @@
+import { Input } from '../../components/input/index.tsx'
+import { Button } from '../../components/button/index.tsx'
+ 
+import { Container, Form } from './styles.ts'
+
+export function Signin(){
+    return (
+        <Container>
+            <div className="food">
+                <svg xmlns="http://www.w3.org/2000/svg" width="43" height="48" viewBox="0 0 43 48" fill="none"><path d="M21.5706 0.216553L42.9737 12.0919V35.8426L21.5706 47.7179L0.167517 35.8426V12.0919L21.5706 0.216553Z" fill="#065E7C"/></svg>
+                <h1>food explorer</h1>    
+            </div>
+            <Form>
+                <h1>Faça login</h1>
+                <div className='div-input'>
+                    <p>Email</p>
+                    <Input name={'Exemplo: exemplo@exemplo.com.br'} />
+                </div>
+                <div className='div-input senha'>
+                    <p>Senha</p>
+                    <Input 
+                    name={'No mínimo 6 caracteres'}
+                    />
+                </div>
+                <Button name={'Criar conta'}/>
+
+                <a href="">Criar uma conta</a>
+            </Form>
+        </Container>
+    )
+}
